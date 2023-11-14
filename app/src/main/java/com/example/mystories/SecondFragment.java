32e9811c017a300240abeeeff555c71a369a2466
lequanphat <lequanphat3579@gmail.com>
2023-11-14 17:05:27 +0700

    take picture

--- app/src/main/java/com/example/mystories/SecondFragment.java
+++ app/src/main/java/com/example/mystories/SecondFragment.java
@@ -1,11 +1,20 @@
 package com.example.mystories;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,21 +24,45 @@ import java.util.ArrayList;
 
 public class SecondFragment extends Fragment {
     private RecyclerView recyclerView;
-
+    private ImageView imageView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    public void initView(View root){
+        imageView = root.findViewById(R.id.image);
+    }
+    public void initEvent(View root){
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntentLauncher.launch(takePictureIntent);
+            }
+        });
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Activity activity = getActivity();
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.second_fragment, container, false);
-
-
-
+        initView(root);
+        initEvent(root);
         return root;
     }
+    public ActivityResultLauncher<Intent> cameraIntentLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+                            imageView.setImageBitmap(capturedImage);
+                        }
+                    }
+                }
+            });
 }
